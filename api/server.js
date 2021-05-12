5de334d5e64051a40be9260dd9f3845d7b4b2c2b
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-12 14:00:32 -0400

    setup dependencies, index, server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const cors = require("cors");
+const helmet = require("helmet");
 const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+server.use(cors());
+server.use(helmet());
+
 
 module.exports = server;
